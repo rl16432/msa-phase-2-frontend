2574a9a5bd95c9e18b9772bab6046c48b144a20e
rl16432 <rluo52237@gmail.com>
2022-07-27 21:16:47 +1200

    Added search function

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,10 @@ import "./App.css";
 import { Button, Container, TextField, Typography } from "@mui/material";
 
 function App() {
-  
-  const OMDB_API_BASE_URL = `http://www.omdbapi.com/`;
+
+  const API_KEY = process.env.REACT_APP_API_KEY;
+  const OMDB_API_BASE_URL = `http://www.omdbapi.com`;
+
   const [searchQuery, setSearchQuery] = useState<string>("");
   const [movies, setMovies] = useState<undefined | any>(undefined);
 
@@ -26,13 +28,50 @@ function App() {
           size="small"
           sx={{ mr: 2 }}
         />
-        <Button variant="contained">Search</Button>
+        <Button variant="contained" onClick={search}>Search</Button>
         <Typography variant="body1" sx={{}}>
           {searchQuery}
         </Typography>
       </Container>
     </div>
   );
+
+  function search() {
+    axios
+      .get(OMDB_API_BASE_URL,
+        {
+          params: {
+            apikey: API_KEY,
+            s: searchQuery
+          }
+        })
+      .then((response) => {
+        let movieInfo = response.data.Search
+        
+        movieInfo = Promise.all(movieInfo.map(
+          (movie: any) => {
+            return axios
+              .get(OMDB_API_BASE_URL,
+                {
+                  params: {
+                    apikey: API_KEY,
+                    i: movie.imdbID
+                  }
+                }
+              )
+              .then(response => {
+                return response.data
+              })
+          }
+        )).then(movieInfo => {
+          console.log(movieInfo)
+          setMovies(movieInfo)
+        })
+      })
+      .catch(error => {
+        console.error(error)
+      })
+  }
 }
 
 export default App;
