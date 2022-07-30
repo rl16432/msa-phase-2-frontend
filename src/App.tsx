f22580aa06991fa1a47749e9060fea0fe910676c
rl16432 <rluo52237@gmail.com>
2022-07-30 21:23:57 +1200

    Adjust global margin/padding

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./App.css";
-import { Button, Container, TextField, Typography } from "@mui/material";
+import { Box, Button, Container, createTheme, TextField, ThemeProvider, Typography } from "@mui/material";
 
 function App() {
 
@@ -9,30 +9,36 @@ function App() {
   const OMDB_API_BASE_URL = `http://www.omdbapi.com`;
 
   const [searchQuery, setSearchQuery] = useState<string>("");
-  const [movies, setMovies] = useState<undefined | any>(undefined);
+  const [movies, setMovies] = useState<undefined | any>([]);
+
+  const theme = createTheme({
+    spacing: (factor: number) => `${0.25 * factor}rem`
+  })
 
   return (
     <div className="App">
-      <Container sx={{ my: 3 }}>
-        <Typography variant="h2" sx={{ my: 3 }}>
-          Movie Search
-        </Typography>
-        <TextField
-          value={searchQuery}
-          onChange={(event: any) => {
-            setSearchQuery(event.target.value);
-          }}
-          label="Search for movie"
-          variant="outlined"
-          placeholder="Search"
-          size="small"
-          sx={{ mr: 2 }}
-        />
-        <Button variant="contained" onClick={search}>Search</Button>
-        <Typography variant="body1" sx={{}}>
-          {searchQuery}
-        </Typography>
-      </Container>
+      <ThemeProvider theme={theme}>
+        <Container sx={{ my: 3 }}>
+          <Typography variant="h2" sx={{ my: 3 }}>
+            Movie Search
+          </Typography>
+          <TextField
+            value={searchQuery}
+            onChange={(event: any) => {
+              setSearchQuery(event.target.value);
+            }}
+            label="Search for movie"
+            variant="outlined"
+            placeholder="Search"
+            size="small"
+            sx={{ mr: 2 }}
+          />
+          <Button variant="contained" onClick={search}>Search</Button>
+          <Typography variant="body1" sx={{}}>
+            {searchQuery}
+          </Typography>
+        </Container>
+      </ThemeProvider>
     </div>
   );
 
@@ -47,7 +53,7 @@ function App() {
         })
       .then((response) => {
         let movieInfo = response.data.Search
-        
+
         movieInfo = Promise.all(movieInfo.map(
           (movie: any) => {
             return axios
