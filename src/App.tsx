4d49f418aee1c8d3024b1a308dc9e8c124ce8999
rl16432 <rluo52237@gmail.com>
2022-08-04 13:38:21 +1200

    Move movie info to separate component

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,66 @@ import {
   Typography,
 } from "@mui/material";
 
+function MovieInfo({ movie }: any) {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "row",
+        flexGrow: "1",
+      }}
+    >
+      <Box
+        textAlign="left"
+        className="movie-text-box"
+        sx={{
+          mr: 3,
+        }}
+      >
+        <Link
+          href={`https://www.imdb.com/title/${movie.imdbID}`}
+          variant="h6"
+          target="_blank"
+          rel="noopener"
+        >
+          <strong>{movie.Title}</strong>
+        </Link>
+        <Typography variant="subtitle1">
+          <strong>Year:</strong> {movie.Year}
+        </Typography>
+        <Typography variant="subtitle1">
+          <strong>Director:</strong> {movie.Director}
+        </Typography>
+        <Typography variant="subtitle1">
+          <strong>Rated:</strong> {movie.Rated}
+        </Typography>
+        <Typography variant="subtitle1">
+          <strong>Box Office:</strong> {movie.BoxOffice}
+        </Typography>
+      </Box>
+
+      <Box textAlign="left" className="movie-text-box">
+        <Typography variant="subtitle1">
+          <strong>Genre:</strong> {movie.Genre}
+        </Typography>
+        <Typography variant="subtitle1">
+          <strong>Runtime:</strong> {movie.Runtime}
+        </Typography>
+        <Typography variant="subtitle1">
+          <strong>Ratings:</strong>
+        </Typography>
+        {movie.Ratings != null
+          ? movie.Ratings.map((rating: any) => (
+              <Typography key={rating.Source} variant="subtitle1">
+                {rating.Source}: {rating.Value}
+              </Typography>
+            ))
+          : null}
+      </Box>
+    </Box>
+  );
+}
+
 function App() {
   const API_KEY = process.env.REACT_APP_API_KEY;
   const OMDB_API_BASE_URL = `http://www.omdbapi.com`;
@@ -50,9 +110,12 @@ function App() {
           <Button variant="contained" onClick={search}>
             Search
           </Button>
+
+          {/* Alert message */}
           {message !== "" && message !== undefined ? (
             <Alert severity="error">{message}</Alert>
           ) : null}
+
           {movies.map((movie: any) => (
             <Box
               sx={{
@@ -69,61 +132,7 @@ function App() {
               }}
               key={movie.imdbID}
             >
-              <Box
-                sx={{
-                  display: "flex",
-                  flexDirection: "row",
-                  flexGrow: "1",
-                }}
-              >
-                <Box
-                  textAlign="left"
-                  className="movie-text-box"
-                  sx={{
-                    mr: 3,
-                  }}
-                >
-                  <Link
-                    href={`https://www.imdb.com/title/${movie.imdbID}`}
-                    variant="h6"
-                    target="_blank"
-                    rel="noopener"
-                  >
-                    <strong>{movie.Title}</strong>
-                  </Link>
-                  <Typography variant="subtitle1">
-                    <strong>Year:</strong> {movie.Year}
-                  </Typography>
-                  <Typography variant="subtitle1">
-                    <strong>Director:</strong> {movie.Director}
-                  </Typography>
-                  <Typography variant="subtitle1">
-                    <strong>Rated:</strong> {movie.Rated}
-                  </Typography>
-                  <Typography variant="subtitle1">
-                    <strong>Box Office:</strong> {movie.BoxOffice}
-                  </Typography>
-                </Box>
-
-                <Box textAlign="left" className="movie-text-box">
-                  <Typography variant="subtitle1">
-                    <strong>Genre:</strong> {movie.Genre}
-                  </Typography>
-                  <Typography variant="subtitle1">
-                    <strong>Runtime:</strong> {movie.Runtime}
-                  </Typography>
-                  <Typography variant="subtitle1">
-                    <strong>Ratings:</strong>
-                  </Typography>
-                  {movie.Ratings != null
-                    ? movie.Ratings.map((rating: any) => (
-                        <Typography key={rating.Source} variant="subtitle1">
-                          {rating.Source}: {rating.Value}
-                        </Typography>
-                      ))
-                    : null}
-                </Box>
-              </Box>
+              <MovieInfo movie={movie} />
               <Box
                 component="img"
                 sx={{
@@ -158,7 +167,7 @@ function App() {
         let movieInfo = response.data.Search;
 
         // Additional requests to get detailed movie data by imdbID
-        movieInfo = Promise.all(
+        Promise.all(
           movieInfo.map((movie: any) => {
             return axios
               .get(OMDB_API_BASE_URL, {
